6a596d127f4181f28025440e80a50efb4263bb40
farvellmaster <farvellsmurf@gmail.com>
2021-05-28 01:34:50 +0200

    added es-lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
-        "es6": true
+        "es5": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {

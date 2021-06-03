148ed68833904fa70f2e30c344681c129d55fc0e
farvellmaster <farvellsmurf@gmail.com>
2021-06-03 05:03:03 +0200

    added Style class

---
+++ src/style.js
@@ -0,0 +1,27 @@
+// Converts style objetcs into html props.
+
+module.exports = class Style {
+	constructor( styles ) {
+		// Object containig style keys
+		this.styles = styles;
+
+		// Return set styles
+		return Object.freeze(Object.create({
+
+			setStyles: this.setStyles.bind( this )
+
+		}));
+
+	}
+
+	// Set styles to targets clasName
+	setStyles( className ) {
+		this.elements = [ ...document.getElementsByClassName( className ) ];
+
+		this.elements.forEach(( element ) => {
+			Object.keys( this.styles ).forEach(( key ) => {
+				element.style.key = this.styles.key;
+			});
+		});
+	}
+}

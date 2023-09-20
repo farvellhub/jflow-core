0d77d7398853784dc1277f50f77bc25c44c2f275
Farvell <farvellsmurf@gmail.com>
2023-09-20 17:56:16 +0200

    Refactorized Parallax class

--- src/animations/parallax.ts
+++ src/animations/parallax.ts
@@ -1,21 +1,20 @@
 import TypeParallax from "../types/type-parallax";
 
-class Parallax {
-    #config: Array<TypeParallax> = [];
+export default class Parallax {
+    #config: TypeParallax[] = [];
 
-    constructor( ...config: Array<TypeParallax> ) {
+    constructor( ...config: TypeParallax[] ) {
 
         // Init multiple configs
         this.initConfig( config );
         // Need updates movement at restart
-        // Prevents charging scroll at re-render position
+        // Prevents charging scroll when re-renders position
         this.updateMovement();
     }
 
     // Sets config params scroll direction, speed , offset
-    private initConfig( config: Array<TypeParallax> ): void {
-        config.forEach(( param: TypeParallax ) => {
-            const { target, direction, offset } = param;
+    private initConfig( config: TypeParallax[] ): void {
+        config.forEach(({ target, direction, offset }: TypeParallax ) => {
             this.#config.push({
                 target: document.getElementById( target as string ) as HTMLElement,
                 direction: ( direction > -2 && direction < 2 )
@@ -29,13 +28,12 @@ class Parallax {
     // Updates movement relative on direction and offset
     private updateMovement( offsetScroll: number = window.scrollY ): void {
         // For each parallax configuration object
-        this.#config.forEach(( value: TypeParallax ) => {
-            const { target, direction, offset } = value;
+        this.#config.forEach(({ target, direction, offset }: TypeParallax ) => {
             const movement = ( offset! - offsetScroll ) * direction;
 
             // Only executes if offset arrives to minOffset
             if ( offsetScroll >= offset! )
-                ( target as HTMLElement ).style.transform = `translateY( ${ movement }px)`;
+                ( target as HTMLElement ).style.transform = `translateY(${ movement }px)`;
         });
     }
 
@@ -47,12 +45,9 @@ class Parallax {
     }
 
     // Event handler for scroll
-    public async listen(): Promise<void> {
+    public listen() {
         window.addEventListener( "scroll", () => {
-            const offset = window.scrollY;
-            this.render( offset );
+            this.render( window.scrollY );
         });
     }
 }
-
-export default Parallax;
